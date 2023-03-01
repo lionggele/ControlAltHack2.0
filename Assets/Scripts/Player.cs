17e89481a3937d5ba81d8f9b6099c6bf3a2cc43e
Gele <lionggele40@gmail.com>
2023-03-02 03:08:59 +0800

    character movement + animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,27 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    public int flag= 0;
-    public float speed = 5.0f;
-    /*
-    public Text flagAmount;
-    public Text Escaped;
-    public GameObject door;
-    */
-    // Start is called before the first frame update
+    public float m_moveSpeed = 3f;
+    private Animator animator;
+    Vector2 m_moveInput = Vector2.zero;
+    Rigidbody2D m_rb;
+
     void Start()
     {
-
+        m_rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
+    private void FixedUpdate()
+    {
+        m_rb.velocity = m_moveInput * m_moveSpeed;
+    }
+
+    void OnMove(InputValue value)
+    {
+        m_moveInput = value.Get<Vector2>();
+
+            animator.SetFloat("Xinput", m_moveInput.x);
+            animator.SetFloat("Yinput", m_moveInput.y);
+        
+    }
+
+}
+ 
+
+    /*
     // Update is called once per frame
     void Update()
     {
+        /*
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Translate(-speed * Time.deltaTime, 0, 0);
@@ -39,7 +55,7 @@ public class Player : MonoBehaviour
             transform.Translate(0, -speed * Time.deltaTime, 0);
         }
 
-        /*
+ 
         if (flag == 3)
         {
             Destroy(door);
@@ -47,7 +63,8 @@ public class Player : MonoBehaviour
         */
        
 
-    }
+    
+    /*
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -78,5 +95,12 @@ public class Player : MonoBehaviour
 
 
 
-    }
-}
+    /*
+    public int flag= 0;
+    public float speed = 5.0f;
+
+    public Text flagAmount;
+    public Text Escaped;
+    public GameObject door;
+    */
+    // Start is called before the first frame update
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*
+        animator = GetComponent<animator>();       
+        8?
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        public void OnMove(InputValue value)
+        {
+            
+            moveInput = value.Get<Vector2>();
+            if (moveInput != Vector2.zero)
+            {
+                animator.SetFloat("Xinput", moveInput.x);
+                animator.SetFloat("Yinput", moveInput.y);
+            }
+        }
+        */
+        
+    }
+}
