a15e2201cee15941d6fdc1add0e94d109b7da01a
Gele <lionggele40@gmail.com>
2023-03-04 00:16:27 +0800

    walking animation + camera changes

---
+++ Assets/Scripts/CameraMotion.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotion : MonoBehaviour
+{
+    public Transform lookAt;
+    public float boundX = 0.15f;
+    public float boundY = 0.05f;
+
+    private void LateUpdate(){
+        Vector3 delta = Vector3.zero;
+
+        float deltaX = lookAt.position.x - transform.position.x;
+        if(deltaX > boundX || deltaX < -boundX)
+        {
+            if(transform.position.x < lookAt.position.x){
+                delta.x = deltaX - boundX;
+            }
+            else{
+                delta.x = deltaX + boundX;
+            }
+        }
+
+
+        // this is to heck if we're inside the bounds on the Y axis 
+        float deltaY = lookAt.position.y - transform.position.y;
+        if(deltaY > boundY || deltaY < -boundY)
+        {
+            if(transform.position.y < lookAt.position.y){
+                delta.y = deltaY - boundY;
+            }
+            else{
+                delta.y = deltaY + boundY;
+            }
+        }
+        
+        transform.position += new Vector3(delta.x,delta.y,0);
+    
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
