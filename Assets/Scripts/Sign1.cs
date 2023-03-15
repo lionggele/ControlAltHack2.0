212cc8040c8c904620969ba695e6b5b06bebaae0
Gele <lionggele40@gmail.com>
2023-03-15 09:13:54 +0800

    password scene

--- Assets/Scripts/Sign1.cs
+++ Assets/Scripts/Sign1.cs
@@ -5,33 +5,29 @@ using UnityEngine.UI;
 using TMPro;
 using System;
 
-public class Sign : MonoBehaviour{
+public class Sign1 : MonoBehaviour{
 
     public GameObject dialogBox;
-    public TMP_Text dialogText;
+
     public string dialog;
     public bool playerInRange;
     
     
 	// Use this for initialization
 	void Start () {
-        dialogText.text = "";
         dialogBox.SetActive(false);
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
         if(playerInRange && Input.GetKeyDown(KeyCode.M))
         {
             if(dialogBox.activeInHierarchy)
             {
                 dialogBox.SetActive(false);
-                dialogText.text = "";
             }else{
                 dialogBox.SetActive(true);
-                dialogText.text = dialog;
             }
         }
         
